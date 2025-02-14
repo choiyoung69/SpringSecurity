e047f04ef4faf5ff1cbbf0dd247b342beeb3ffe3
choiyoung69 <choiyoung609@gmail.com>
2025-02-14 13:37:58 +0900

    [스프링 시큐리티 인 액션 2장] 예제 1번

---
+++ ex1/src/main/java/springsecurity/ex1/config/ProjectConfig.java
@@ -0,0 +1,50 @@
+package springsecurity.ex1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("John")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        auth ->
+                                auth
+                                .anyRequest()
+                                .authenticated());
+
+
+        return httpSecurity.build();
+    }
+}
